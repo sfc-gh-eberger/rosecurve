ab7b6299cd664c05f2745a102771ad1f189fd075
sfc-gh-eberger <142522520+sfc-gh-eberger@users.noreply.github.com>
2023-11-21 20:59:02 +0000

    Commit

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -1,77 +1,39 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from urllib.error import URLError
-
-import altair as alt
-import pandas as pd
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import matplotlib.pyplot as plt
+import numpy as np
 
+# Function to generate rhodonea curve data
+def rhodonea_curve(n, d, theta_values):
+    r = np.cos(n * theta_values) + np.sin(d * theta_values)
+    return r
 
-def data_frame_demo():
-    @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+# Streamlit app
+def main():
+    st.title('Rhodonea Curve Graphing')
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
-        )
-        if not countries:
-            st.error("Please select at least one country.")
-        else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+    n = st.slider('Value of n', -20.0, 20.0, 5.0, step=0.25)
+    d = st.slider('Value of d', -20.0, 20.0, 7.0, step=0.25)
 
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+    color = 'red'
 
+    # Radio button for filling the center with red color
+    fill_color = st.radio("Fill the center with red color", ('Yes', 'No'))
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
-st.markdown("# DataFrame Demo")
-st.sidebar.header("DataFrame Demo")
-st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
-)
+    theta_min = 0
+    theta_max = 2 * np.pi
 
-data_frame_demo()
+    theta_values = np.linspace(theta_min, theta_max, 1000)
+    r_values = rhodonea_curve(n, d, theta_values)
 
-show_code(data_frame_demo)
+    # Plotting the rhodonea curve
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'projection': 'polar'})
+    ax.plot(theta_values, r_values, color=color)
+
+    if fill_color == 'Yes':
+        ax.fill(theta_values, r_values, color='red', alpha=0.3)
+
+    ax.set_title(f'Rhodonea Curve with n={n}, d={d}')
+    st.pyplot(fig)
+
+if __name__ == '__main__':
+    main()
